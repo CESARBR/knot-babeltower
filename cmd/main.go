0d820e248175715e1b9f070ea8c48b07969bc19d
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-11 18:03:08 -0300

    amqp: Add handler
    This patch adds the amqp connection handler.

--- cmd/main.go
+++ cmd/main.go
@@ -17,9 +17,21 @@ func main() {
 	logger := logrus.Get("Main")
 	logger.Info("Starting KNoT Babeltower")
 
+	// AMQP
+	amqp := network.NewAmqp(logrus.Get("Amqp"))
+
+	// Services
 	userProxy := network.NewUserProxy(logrus.Get("UserProxy"), config.Users.Hostname, config.Users.Port)
+
+	// Interactors
 	createUser := interactors.NewCreateUser(logrus.Get("CreateUser"), userProxy)
+
+	// Controllers
 	userController := controllers.NewUserController(logrus.Get("Controller"), createUser)
+
+	// Server
 	server := server.NewServer(config.Server.Port, logrus.Get("Server"), userController)
+
+	amqp.Start()
 	server.Start()
 }
