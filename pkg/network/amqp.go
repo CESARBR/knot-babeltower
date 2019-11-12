ed9ec98a6cf8a3d578304d3cb2768fb7dcdc78a0
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-12 08:10:39 -0300

    amqp: Establish connection on Start
    This patch keeps a connection active to RabbitMQ.

--- pkg/network/amqp.go
+++ pkg/network/amqp.go
@@ -1,20 +1,49 @@
 package network
 
-import "github.com/CESARBR/knot-babeltower/pkg/logging"
+import (
+	"github.com/CESARBR/knot-babeltower/pkg/logging"
+	"github.com/streadway/amqp"
+)
 
 // Amqp handles the connection, queues and exchanges declared
 type Amqp struct {
-	url    string
-	logger logging.Logger
+	url     string
+	logger  logging.Logger
+	conn    *amqp.Connection
+	channel *amqp.Channel
+}
+
+func (a *Amqp) notifyWhenClosed() {
+	errReason := <-a.conn.NotifyClose(make(chan *amqp.Error))
+	a.logger.Infof("AMQP connection closed: %s", errReason)
+	// TODO: try to reconnect
 }
 
 // NewAmqp constructs the AMQP connection handler
 func NewAmqp(url string, logger logging.Logger) *Amqp {
-	return &Amqp{url, logger}
+	return &Amqp{url, logger, nil, nil}
 }
 
 // Start starts the handler
-func (ah *Amqp) Start() {
-	ah.logger.Debug("AMQP handler started")
-	// TODO: Start amqp connection
+func (a *Amqp) Start() {
+	a.logger.Debug("AMQP handler started")
+	conn, err := amqp.Dial(a.url)
+	if err != nil {
+		// TODO: try to reconnect
+		a.logger.Error(err)
+		return
+	}
+
+	a.conn = conn
+	go a.notifyWhenClosed()
+
+	channel, err := conn.Channel()
+	if err != nil {
+		// TODO: try to create channel again
+		a.logger.Error(err)
+		return
+	}
+
+	a.logger.Debug("AMQP handler connected")
+	a.channel = channel
 }
