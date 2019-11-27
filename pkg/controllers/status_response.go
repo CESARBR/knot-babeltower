fbc17472662f7bcec49d08610c37b3357cbc4545
João Neto <joao.alexandre@cesar.org.br>
2019-11-27 10:43:11 -0300

    user: crtl: Refactor writeResponse to generalize

--- pkg/controllers/status_response.go
+++
@@ -1,6 +0,0 @@
-package controllers
-
-// StatusResponse represents the response to be sent to the request
-type StatusResponse struct {
-	Message string `json:"message"`
-}
--- pkg/controllers/user.go
+++ pkg/controllers/user.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/CESARBR/knot-babeltower/pkg/logging"
@@ -24,23 +23,33 @@ func NewUserController(
 	return &UserController{logger, createUserInteractor}
 }
 
-func (uc *UserController) writeResponse(w http.ResponseWriter, status int, err string) {
-	js, jsonErr := json.Marshal(StatusResponse{Message: err})
-	if jsonErr != nil {
-		uc.logger.Errorf("Unable to marshal json: %s", jsonErr)
+// DetailedErrorResponse represents the response to be sent to the request
+type DetailedErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func (uc *UserController) writeResponse(w http.ResponseWriter, statusCode int, msg interface{}) {
+	w.WriteHeader(statusCode)
+
+	if msg == nil {
 		return
 	}
 
-	w.WriteHeader(status)
+	js, err := json.Marshal(msg)
+	if err != nil {
+		uc.logger.Errorf("Unable to marshal json: %s", err)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	_, writeErr := w.Write(js)
-	if writeErr != nil {
-		uc.logger.Errorf("Unable to write to connection HTTP: %s", writeErr)
+	_, err = w.Write(js)
+	if err != nil {
+		uc.logger.Errorf("Unable to write to connection HTTP: %s", err)
 		return
 	}
 }
 
-func verifyErrorType(err error) int {
+func mapErrorToStatusCode(err error) int {
 	switch err.(type) {
 	case entities.ErrEntityExists:
 		return http.StatusConflict
@@ -63,28 +72,25 @@ func verifyErrorType(err error) int {
 func (uc *UserController) Create(w http.ResponseWriter, r *http.Request) {
 	var err error
 	var user entities.User
-	var decoder *json.Decoder
 
 	uc.logger.Debug("Handle request to create user")
 
-	decoder = json.NewDecoder(r.Body)
-
+	decoder := json.NewDecoder(r.Body)
 	err = decoder.Decode(&user)
 	if err != nil {
-		errStr := fmt.Sprintf("Invalid request format: %s", err)
-		uc.logger.Error(errStr)
-		uc.writeResponse(w, http.StatusUnprocessableEntity, errStr)
+		uc.logger.Error("Failed to parse request body")
+		uc.writeResponse(w, http.StatusUnprocessableEntity, nil)
 		return
 	}
 
 	err = uc.createUserInteractor.Execute(user)
 	if err != nil {
-		uc.logger.Errorf("Response error: %s", err)
-		uc.writeResponse(w, verifyErrorType(err), err.Error())
+		uc.logger.Errorf("Failed to create user")
+		der := &DetailedErrorResponse{err.Error()}
+		uc.writeResponse(w, mapErrorToStatusCode(err), der)
 		return
 	}
 
-	msg := fmt.Sprintf("User %s created", user.Email)
-	uc.logger.Info(msg)
-	uc.writeResponse(w, http.StatusCreated, msg)
+	uc.logger.Infof("User %s created", user.Email)
+	uc.writeResponse(w, http.StatusCreated, nil)
 }
