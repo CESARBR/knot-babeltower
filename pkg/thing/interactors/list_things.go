94743885b63647f311389d16eedf48769065cb0c
João Neto <joao.alexandre@cesar.org.br>
2020-01-17 16:26:11 -0300

    thing: interactor: Add list things
    The use case necessary to list the user's registered things only
    requires getting them from the thing's service and publishing them
    on the appropriate message queue. Hence, this patch orchestrate the
    execution of the operations provided by both external dependencies
    so that it can be accomplished.

---
+++ pkg/thing/interactors/list_things.go
@@ -0,0 +1,25 @@
+package interactors
+
+import (
+	"errors"
+)
+
+// List fetchs the registered things and return them as an array
+func (i *ThingInteractor) List(authorization string) error {
+	if authorization == "" {
+		return errors.New("authorization key not provided")
+	}
+
+	things, err := i.thingProxy.List(authorization)
+	if err != nil {
+		return err
+	}
+
+	err = i.msgPublisher.SendThings(things)
+	if err != nil {
+		return err
+	}
+
+	i.logger.Info("devices obtained")
+	return nil
+}
