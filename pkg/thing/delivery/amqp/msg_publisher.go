1e2afb24976a20b3e59247961413af35ddb320b5
João Neto <joao.alexandre@cesar.org.br>
2020-01-17 16:20:50 -0300

    thing: publisher: Send list things response
    The registered things response should be sent to the clients through
    a message published on the broker. So, this patch adds an operation to
    publish them based on "device.list" binding key.

--- pkg/thing/delivery/amqp/msg_publisher.go
+++ pkg/thing/delivery/amqp/msg_publisher.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/CESARBR/knot-babeltower/pkg/logging"
 	"github.com/CESARBR/knot-babeltower/pkg/network"
+	"github.com/CESARBR/knot-babeltower/pkg/thing/entities"
 )
 
 const (
-	exchangeFogOut = "fogOut"
-	registerOutKey = "device.registered"
-	schemaOutKey   = "schema.updated"
+	exchangeFogOut   = "fogOut"
+	registerOutKey   = "device.registered"
+	schemaOutKey     = "schema.updated"
+	listThingsOutKey = "device.list"
 )
 
 // MsgPublisher handle messages received from a service
@@ -23,6 +25,7 @@ type MsgPublisher struct {
 type Publisher interface {
 	SendRegisterDevice(network.RegisterResponseMsg) error
 	SendUpdatedSchema(thingID string) error
+	SendThings(things []*entities.Thing) error
 }
 
 // NewMsgPublisher constructs the MsgPublisher
@@ -52,4 +55,15 @@ func (mp *MsgPublisher) SendUpdatedSchema(thingID string) error {
 	}
 
 	return mp.amqp.PublishPersistentMessage(exchangeFogOut, schemaOutKey, msg)
+}
+
+// SendThings sends the updated schema response
+func (mp *MsgPublisher) SendThings(things []*entities.Thing) error {
+	resp := &network.ListThingsResponse{Things: things}
+	msg, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+
+	return mp.amqp.PublishPersistentMessage(exchangeFogOut, listThingsOutKey, msg)
 }
