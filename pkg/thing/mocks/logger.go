d55094371d2371b241e5956bb78689b6a6f2a976
Esdras Júnior <eblsj@cesar.org.br>
2020-02-17 14:06:34 -0300

    thing: interactor: Add unregister device command
    This patch adds the use case interactor that implements the business
    logic to remove things on the thing's service. In addition, it also adds
    the necessary unit tests.

--- pkg/thing/mocks/logger.go
+++ pkg/thing/mocks/logger.go
@@ -1,7 +1,11 @@
 package mocks
 
+import "github.com/stretchr/testify/mock"
+
 // FakeLogger represents a mocking type for the logging service
-type FakeLogger struct{}
+type FakeLogger struct{
+	mock.Mock
+}
 
 // Info provides a mock function for the debugging Info capability
 func (fl *FakeLogger) Info(...interface{}) {}
