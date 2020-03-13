fcbe8b326f132542b0354e2f4a335d5d6b7660aa
Leonardo Melo <lcbm@cesar.school>
2020-03-13 14:59:02 -0300

    thing: interactor: Reorder functions
    Reorder functions and types inside the file in order to standardize
    code style throughout the project.

--- pkg/thing/interactors/update_schema.go
+++ pkg/thing/interactors/update_schema.go
@@ -14,14 +14,43 @@ func (eis *ErrInvalidSchema) Error() string {
 	return "Thing's schema is invalid"
 }
 
+type schemaType struct {
+	valueType interface{}
+	unit      interface{}
+}
+
 type interval struct {
 	min int
 	max int
 }
 
-type schemaType struct {
-	valueType interface{}
-	unit      interface{}
+var rules = map[int]schemaType{
+	0:      schemaType{valueType: interval{1, 4}, unit: 0},
+	0xfff0: schemaType{valueType: interval{1, 4}, unit: 0},
+	0xfff1: schemaType{valueType: interval{1, 4}, unit: 0},
+	0xfff2: schemaType{valueType: interval{1, 4}, unit: 0},
+	0xff10: schemaType{valueType: interval{1, 4}, unit: 0},
+	1:      schemaType{valueType: 1, unit: interval{1, 3}},
+	2:      schemaType{valueType: 1, unit: interval{1, 2}},
+	3:      schemaType{valueType: 1, unit: 1},
+	4:      schemaType{valueType: 1, unit: interval{1, 3}},
+	5:      schemaType{valueType: 1, unit: interval{1, 3}},
+	6:      schemaType{valueType: 1, unit: 1},
+	7:      schemaType{valueType: 1, unit: interval{1, 3}},
+	8:      schemaType{valueType: 1, unit: interval{1, 3}},
+	9:      schemaType{valueType: 1, unit: interval{1, 4}},
+	0x0A:   schemaType{valueType: 1, unit: interval{1, 3}},
+	0x0B:   schemaType{valueType: 1, unit: interval{1, 4}},
+	0x0C:   schemaType{valueType: 2, unit: interval{1, 2}},
+	0x0D:   schemaType{valueType: 2, unit: interval{1, 4}},
+	0x0E:   schemaType{valueType: 2, unit: interval{1, 3}},
+	0x0F:   schemaType{valueType: 2, unit: 1},
+	0x10:   schemaType{valueType: 2, unit: 1},
+	0x11:   schemaType{valueType: 2, unit: 1},
+	0x12:   schemaType{valueType: 2, unit: 1},
+	0x13:   schemaType{valueType: 1, unit: interval{1, 4}},
+	0x14:   schemaType{valueType: 2, unit: interval{1, 6}},
+	0x15:   schemaType{valueType: 1, unit: interval{1, 6}},
 }
 
 // UpdateSchema receive the new sensor schema and update it on the thing's service
@@ -65,33 +94,23 @@ func (i *ThingInteractor) isValidSchema(schemaList []entities.Schema) bool {
 	return true
 }
 
-var rules = map[int]schemaType{
-	0:      schemaType{valueType: interval{1, 4}, unit: 0},
-	0xfff0: schemaType{valueType: interval{1, 4}, unit: 0},
-	0xfff1: schemaType{valueType: interval{1, 4}, unit: 0},
-	0xfff2: schemaType{valueType: interval{1, 4}, unit: 0},
-	0xff10: schemaType{valueType: interval{1, 4}, unit: 0},
-	1:      schemaType{valueType: 1, unit: interval{1, 3}},
-	2:      schemaType{valueType: 1, unit: interval{1, 2}},
-	3:      schemaType{valueType: 1, unit: 1},
-	4:      schemaType{valueType: 1, unit: interval{1, 3}},
-	5:      schemaType{valueType: 1, unit: interval{1, 3}},
-	6:      schemaType{valueType: 1, unit: 1},
-	7:      schemaType{valueType: 1, unit: interval{1, 3}},
-	8:      schemaType{valueType: 1, unit: interval{1, 3}},
-	9:      schemaType{valueType: 1, unit: interval{1, 4}},
-	0x0A:   schemaType{valueType: 1, unit: interval{1, 3}},
-	0x0B:   schemaType{valueType: 1, unit: interval{1, 4}},
-	0x0C:   schemaType{valueType: 2, unit: interval{1, 2}},
-	0x0D:   schemaType{valueType: 2, unit: interval{1, 4}},
-	0x0E:   schemaType{valueType: 2, unit: interval{1, 3}},
-	0x0F:   schemaType{valueType: 2, unit: 1},
-	0x10:   schemaType{valueType: 2, unit: 1},
-	0x11:   schemaType{valueType: 2, unit: 1},
-	0x12:   schemaType{valueType: 2, unit: 1},
-	0x13:   schemaType{valueType: 1, unit: interval{1, 4}},
-	0x14:   schemaType{valueType: 2, unit: interval{1, 6}},
-	0x15:   schemaType{valueType: 1, unit: interval{1, 6}},
+func schemaValidation(sl validator.StructLevel) {
+	schema := sl.Current().Interface().(entities.Schema)
+	typeID := schema.TypeID
+
+	if (typeID < 0 || 15 < typeID) && (typeID < 0xfff0 || 0xfff2 < typeID) && typeID != 0xff10 {
+		sl.ReportError(schema, "schema", "Type ID", "typeID", "false")
+		return
+	}
+
+	if !isValidValueType(schema.TypeID, schema.ValueType) {
+		sl.ReportError(schema, "schema", "Value Type", "valueType", "false")
+		return
+	}
+
+	if !isValidUnit(schema.TypeID, schema.Unit) {
+		sl.ReportError(schema, "schema", "Unit", "unit", "false")
+	}
 }
 
 func isValidValueType(typeID, valueType int) bool {
@@ -136,23 +155,4 @@ func isValidUnit(typeID, unit int) bool {
 	}
 
 	return true
-}
-
-func schemaValidation(sl validator.StructLevel) {
-	schema := sl.Current().Interface().(entities.Schema)
-	typeID := schema.TypeID
-
-	if (typeID < 0 || 15 < typeID) && (typeID < 0xfff0 || 0xfff2 < typeID) && typeID != 0xff10 {
-		sl.ReportError(schema, "schema", "Type ID", "typeID", "false")
-		return
-	}
-
-	if !isValidValueType(schema.TypeID, schema.ValueType) {
-		sl.ReportError(schema, "schema", "Value Type", "valueType", "false")
-		return
-	}
-
-	if !isValidUnit(schema.TypeID, schema.Unit) {
-		sl.ReportError(schema, "schema", "Unit", "unit", "false")
-	}
 }
