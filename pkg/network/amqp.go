53f6de47ed134db7bd88f1f991423f20a05d6370
Leonardo Melo <lcbm@cesar.school>
2020-03-16 18:50:07 -0300

    refactor: network: Reorder ampq functions
    In order to standardize code throughout the project, reorder the
    functions so that the order of appearence in file is the following:
    
    	1. constant declaration
    	2. variable declaration
    	3. interface declaration
    	4. type declaration
    	5. newXYZ()/NewXYZ() functions
    	6. exported functions
    	7. private functions
    	8. plain utility functions

--- pkg/network/amqp.go
+++ pkg/network/amqp.go
@@ -24,76 +24,6 @@ type InMsg struct {
 	Body       []byte
 }
 
-func (a *Amqp) notifyWhenClosed(started chan bool) {
-	errReason := <-a.conn.NotifyClose(make(chan *amqp.Error))
-	a.logger.Infof("AMQP connection closed: %s", errReason)
-	started <- false
-	if errReason != nil {
-		err := backoff.Retry(a.connect, backoff.NewExponentialBackOff())
-		if err != nil {
-			a.logger.Error(err)
-			started <- false
-			return
-		}
-
-		go a.notifyWhenClosed(started)
-		started <- true
-	}
-}
-
-func (a *Amqp) connect() error {
-	conn, err := amqp.Dial(a.url)
-	if err != nil {
-		a.logger.Error(err)
-		return err
-	}
-
-	a.conn = conn
-
-	channel, err := a.conn.Channel()
-	if err != nil {
-		a.logger.Error(err)
-		return err
-	}
-
-	a.logger.Debug("AMQP handler connected")
-	a.channel = channel
-
-	return nil
-}
-
-func (a *Amqp) declareExchange(name string) error {
-	return a.channel.ExchangeDeclare(
-		name,
-		amqp.ExchangeTopic, // type
-		true,               // durable
-		false,              // delete when complete
-		false,              // internal
-		false,              // noWait
-		nil,                // arguments
-	)
-}
-
-func (a *Amqp) declareQueue(name string) error {
-	queue, err := a.channel.QueueDeclare(
-		name,
-		true,  // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // noWait
-		nil,   // arguments
-	)
-
-	a.queue = &queue
-	return err
-}
-
-func convertDeliveryToInMsg(deliveries <-chan amqp.Delivery, outMsg chan InMsg) {
-	for d := range deliveries {
-		outMsg <- InMsg{d.Exchange, d.RoutingKey, d.Headers, d.Body}
-	}
-}
-
 // NewAmqp constructs the AMQP connection handler
 func NewAmqp(url string, logger logging.Logger) *Amqp {
 	return &Amqp{url, logger, nil, nil, nil}
@@ -110,6 +40,57 @@ func (a *Amqp) Start(started chan bool) {
 
 	go a.notifyWhenClosed(started)
 	started <- true
+}
+
+// Stop closes the connection started
+func (a *Amqp) Stop() {
+	if a.conn != nil && !a.conn.IsClosed() {
+		a.conn.Close()
+	}
+
+	if a.channel != nil {
+		a.channel.Close()
+	}
+
+	a.logger.Debug("AMQP handler stopped")
+}
+
+// PublishPersistentMessage sends a persistent message to RabbitMQ
+func (a *Amqp) PublishPersistentMessage(exchange, key string, body []byte) error {
+	err := a.channel.ExchangeDeclare(
+		exchange,
+		amqp.ExchangeTopic, // type
+		true,               // durable
+		false,              // delete when complete
+		false,              // internal
+		false,              // noWait
+		nil,                // arguments
+	)
+	if err != nil {
+		a.logger.Error(err)
+		return err
+	}
+
+	err = a.channel.Publish(
+		exchange,
+		key,
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			Headers:         amqp.Table{},
+			ContentType:     "text/plain",
+			ContentEncoding: "",
+			Body:            body,
+			DeliveryMode:    amqp.Persistent,
+			Priority:        0,
+		},
+	)
+	if err != nil {
+		a.logger.Error(err)
+		return err
+	}
+
+	return nil
 }
 
 // OnMessage receive messages and put them on channel
@@ -157,10 +138,47 @@ func (a *Amqp) OnMessage(msgChan chan InMsg, queueName, exchangeName, key string
 	return nil
 }
 
-// PublishPersistentMessage sends a persistent message to RabbitMQ
-func (a *Amqp) PublishPersistentMessage(exchange, key string, body []byte) error {
-	err := a.channel.ExchangeDeclare(
-		exchange,
+func (a *Amqp) connect() error {
+	conn, err := amqp.Dial(a.url)
+	if err != nil {
+		a.logger.Error(err)
+		return err
+	}
+
+	a.conn = conn
+
+	channel, err := a.conn.Channel()
+	if err != nil {
+		a.logger.Error(err)
+		return err
+	}
+
+	a.logger.Debug("AMQP handler connected")
+	a.channel = channel
+
+	return nil
+}
+
+func (a *Amqp) notifyWhenClosed(started chan bool) {
+	errReason := <-a.conn.NotifyClose(make(chan *amqp.Error))
+	a.logger.Infof("AMQP connection closed: %s", errReason)
+	started <- false
+	if errReason != nil {
+		err := backoff.Retry(a.connect, backoff.NewExponentialBackOff())
+		if err != nil {
+			a.logger.Error(err)
+			started <- false
+			return
+		}
+
+		go a.notifyWhenClosed(started)
+		started <- true
+	}
+}
+
+func (a *Amqp) declareExchange(name string) error {
+	return a.channel.ExchangeDeclare(
+		name,
 		amqp.ExchangeTopic, // type
 		true,               // durable
 		false,              // delete when complete
@@ -168,42 +186,24 @@ func (a *Amqp) PublishPersistentMessage(exchange, key string, body []byte) error
 		false,              // noWait
 		nil,                // arguments
 	)
-	if err != nil {
-		a.logger.Error(err)
-		return err
-	}
-
-	err = a.channel.Publish(
-		exchange,
-		key,
-		false, // mandatory
-		false, // immediate
-		amqp.Publishing{
-			Headers:         amqp.Table{},
-			ContentType:     "text/plain",
-			ContentEncoding: "",
-			Body:            body,
-			DeliveryMode:    amqp.Persistent,
-			Priority:        0,
-		},
-	)
-	if err != nil {
-		a.logger.Error(err)
-		return err
-	}
-
-	return nil
 }
 
-// Stop closes the connection started
-func (a *Amqp) Stop() {
-	if a.conn != nil && !a.conn.IsClosed() {
-		a.conn.Close()
-	}
+func (a *Amqp) declareQueue(name string) error {
+	queue, err := a.channel.QueueDeclare(
+		name,
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
 
-	if a.channel != nil {
-		a.channel.Close()
-	}
+	a.queue = &queue
+	return err
+}
 
-	a.logger.Debug("AMQP handler stopped")
+func convertDeliveryToInMsg(deliveries <-chan amqp.Delivery, outMsg chan InMsg) {
+	for d := range deliveries {
+		outMsg <- InMsg{d.Exchange, d.RoutingKey, d.Headers, d.Body}
+	}
 }
