5947dc73e74be3b1c5f7d91e68fc23054233f012
Esdras Júnior <eblsj@cesar.org.br>
2020-03-20 20:00:34 -0300

    thing: entities: Create a file to define errors
    In order to standardize the errors in things folder, this patch adds a
    file named error.go to define the common errors used inside the folder.

--- pkg/thing/entities/err_thing_forbidden.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-// ErrThingForbidden represents the error when thing cannot be authenticated
-type ErrThingForbidden struct{}
-
-func (etf ErrThingForbidden) Error() string {
-	return "Forbidden to authenticate thing"
-}
