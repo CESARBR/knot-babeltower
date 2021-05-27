2ed52309173f66e0cd20d9b5e1eee4874766a43b
João Neto <joao.alexandre@cesar.org.br>
2021-05-27 10:52:30 -0300

    thing: proxy: Improve readability and docstrings
    A few inconsitencies were found in the `thing_proxy.go`, requiring a
    refactor to improve documentation and maintenability. This refactor
    includes the following changes:
    
    - Update docstrings to be more detailed
    - Change order of language items (struct, functions)
    - Remove excessive logging from the proxy methods
    - Rename some helper structures to be clearer
    - Prevent some structures from being externally exposed

--- pkg/thing/delivery/http/thing_proxy.go
+++ pkg/thing/delivery/http/thing_proxy.go
@@ -18,78 +18,79 @@ func (err errorConflict) Error() string {
 	return "error conflict"
 }
 
-// ThingProxy proxy a request to the thing service interface
+// ThingProxy is an interface to the Mainflux Things service, which provides an API for
+// managing things (logical representation of a physical thing in IoT). This interface
+// provides a set of operations to manage things (CRUD). In addition, it supports the
+// updating of thing's configuration by using the Mainflux metadata capabilities.
+// https://github.com/mainflux/mainflux/blob/0.12.1/things/openapi.yml
 type ThingProxy interface {
-	Create(id, name, authorization string) (idGenerated string, err error)
+	Create(id, name, authorization string) (string, error)
 	UpdateConfig(authorization, ID string, configList []entities.Config) error
 	List(authorization string) (things []*entities.Thing, err error)
 	Get(authorization, ID string) (*entities.Thing, error)
 	Remove(authorization, ID string) error
 }
 
-// ThingProxyRepr is the entity that represents the thing on the remote thing's service
-type ThingProxyRepr struct {
-	ID       string      `json:"id"`
-	Name     string      `json:"name"`
-	Metadata objMetadata `json:"metadata"`
-}
-
-type objMetadata struct {
-	Knot objKnot `json:"knot"`
-}
-
-type objKnot struct {
-	ID     string            `json:"id"`
-	Config []entities.Config `json:"config,omitempty"`
-}
-
-type pageFetchInput struct {
-	Total  int               `json:"total"`
-	Offset int               `json:"offset"`
-	Limit  int               `json:"limit"`
-	Things []*ThingProxyRepr `json:"things"`
-}
-
-type proxy struct {
+type thingProxy struct {
 	url    string
 	logger logging.Logger
 }
 
-// RequestInfo aims to group all request releated information
-type RequestInfo struct {
+type pageFetchSchema struct {
+	Total  int            `json:"total"`
+	Offset int            `json:"offset"`
+	Limit  int            `json:"limit"`
+	Things []*thingSchema `json:"things"`
+}
+
+type thingSchema struct {
+	ID       string        `json:"id"`
+	Name     string        `json:"name"`
+	Metadata thingMetadata `json:"metadata"`
+}
+
+type thingMetadata struct {
+	Thing knotThing `json:"knot"`
+}
+
+type knotThing struct {
+	ID     string            `json:"id"`
+	Config []entities.Config `json:"config,omitempty"`
+}
+
+type requestInfo struct {
 	method        string
 	url           string
 	authorization string
 	contentType   string
 	data          []byte
-	options       *RequestOptions
+	options       *requestOptions
 }
 
-// RequestOptions represents the request query parameters
-type RequestOptions struct {
+type requestOptions struct {
 	Limit  int `url:"limit"`
 	Offset int `url:"offset"`
 }
 
-// NewThingProxy creates a proxy to the thing service
-func NewThingProxy(logger logging.Logger, hostname, protocol string, port uint16) ThingProxy {
+// NewThingProxy creates a new things instance and returns a pointer to the ThingsProxy interface
+// implementation.
+func NewThingProxy(logger logging.Logger, hostname, protocol string, port uint16) *thingProxy {
 	url := fmt.Sprintf("%s://%s:%d", protocol, hostname, port)
-
-	logger.Debug("proxy setup to " + url)
-	return proxy{url, logger}
+	logger.Debug("things proxy configured to " + url)
+	return &thingProxy{url, logger}
 }
 
-// Create register a thing on service and return the id generated
-func (p proxy) Create(id, name, authorization string) (idGenerated string, err error) {
-	p.logger.Debug("proxying request to create thing")
-	t := p.getRemoteThingRepr(id, name, nil)
+// Create registers a new thing in the Mainflux platform. It receives the thing's properties and
+// map them to the Mainflux internal representation. As a result, the operation returns the things
+// ID.
+func (p thingProxy) Create(id, name, authorization string) (string, error) {
+	t := p.getThingSchema(id, name, nil)
 	body, err := json.Marshal(t)
 	if err != nil {
-		p.logger.Error(err)
 		return "", err
 	}
 
-	requestInfo := &RequestInfo{
+	requestInfo := &requestInfo{
 		"POST",
 		p.url + "/things",
 		authorization,
@@ -100,39 +101,37 @@ func (p proxy) Create(id, name, authorization string) (idGenerated string, err e
 
 	resp, err := p.sendRequest(requestInfo)
 	if err != nil {
-		p.logger.Error(err)
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	err = p.mapErrorFromStatusCode(resp.StatusCode)
 	if err != nil {
-		p.logger.Error(err)
 		return "", err
 	}
 
-	locationHeader := resp.Header.Get("Location")
-	thingID := locationHeader[len("/things/"):] // get substring after "/things/"
-	return thingID, nil
+	location := resp.Header.Get("Location")
+	return location[len("/things/"):], nil // get substring after "/things/"
 }
 
-// UpdateConfig receives as parameters the authorization token, thing's ID and config. After that,
-// it sends a HTTP request to the thing's service in order to update it with the new config.
-func (p proxy) UpdateConfig(authorization, ID string, configList []entities.Config) error {
+// UpdateConfig updates the internal thing's representation with the config in the format supported
+// by the KNoT protocol. KNoT Thing config has two data structures: (1) schema and (2) event.
+// (1) represents the sensor semantic models (temperature, voltage, etc).
+// (2) represents the sensor data publishing configuration (interval, custom behavior when data changes, etc).
+func (p thingProxy) UpdateConfig(authorization, ID string, configList []entities.Config) error {
 	t, err := p.Get(authorization, ID)
 	if err != nil {
 		return err
 	}
 
-	rt := p.getRemoteThingRepr(t.ID, t.Name, t.Config)
-	rt.Metadata.Knot.Config = configList
+	rt := p.getThingSchema(t.ID, t.Name, t.Config)
+	rt.Metadata.Thing.Config = configList
 	parsedBody, err := json.Marshal(rt)
 	if err != nil {
-		p.logger.Error(err)
 		return err
 	}
 
-	requestInfo := &RequestInfo{
+	requestInfo := &requestInfo{
 		"PUT",
 		p.url + "/things/" + t.Token,
 		authorization,
@@ -143,7 +142,6 @@ func (p proxy) UpdateConfig(authorization, ID string, configList []entities.Conf
 
 	resp, err := p.sendRequest(requestInfo)
 	if err != nil {
-		p.logger.Error(err)
 		return err
 	}
 	defer resp.Body.Close()
@@ -151,7 +149,10 @@ func (p proxy) UpdateConfig(authorization, ID string, configList []entities.Conf
 	return p.mapErrorFromStatusCode(resp.StatusCode)
 }
 
-func (p proxy) List(authorization string) ([]*entities.Thing, error) {
+// List returns the registered things according to the KNoT Cloud representation.
+// The Mainflux Things API blocks requests for a large number of things. Thus,
+// this method paginates over them a returns a single slice of things.
+func (p thingProxy) List(authorization string) ([]*entities.Thing, error) {
 	things := []*entities.Thing{}
 	pagThings, err := p.getPaginatedThings(authorization)
 	if err != nil {
@@ -159,14 +160,14 @@ func (p proxy) List(authorization string) ([]*entities.Thing, error) {
 	}
 
 	for _, t := range pagThings {
-		things = append(things, &entities.Thing{ID: t.Metadata.Knot.ID, Name: t.Name, Config: t.Metadata.Knot.Config})
+		things = append(things, &entities.Thing{ID: t.Metadata.Thing.ID, Name: t.Name, Config: t.Metadata.Thing.Config})
 	}
 
 	return things, err
 }
 
-// Get list the things registered on thing's service
-func (p proxy) Get(authorization, ID string) (*entities.Thing, error) {
+// Get retrieves an invidual thing from the Mainflux service. It uses the KNoT Thing's ID as filter.
+func (p thingProxy) Get(authorization, ID string) (*entities.Thing, error) {
 	things, err := p.getPaginatedThings(authorization)
 	if err != nil {
 		return nil, err
@@ -174,8 +175,8 @@ func (p proxy) Get(authorization, ID string) (*entities.Thing, error) {
 
 	for i := range things {
 		t := things[i]
-		if t.Metadata.Knot.ID == ID {
-			nt := &entities.Thing{ID: ID, Token: t.ID, Name: t.Name, Config: t.Metadata.Knot.Config}
+		if t.Metadata.Thing.ID == ID {
+			nt := &entities.Thing{ID: ID, Token: t.ID, Name: t.Name, Config: t.Metadata.Thing.Config}
 			return nt, nil
 		}
 	}
@@ -183,14 +184,14 @@ func (p proxy) Get(authorization, ID string) (*entities.Thing, error) {
 	return nil, entities.ErrThingNotFound
 }
 
-// Remove removes the indicated thing from the thing's service
-func (p proxy) Remove(authorization, ID string) error {
+// Remove removes an individual thing from the Mainflux service. It uses the KNoT Thing's ID as filter.
+func (p thingProxy) Remove(authorization, ID string) error {
 	t, err := p.Get(authorization, ID)
 	if err != nil {
 		return err
 	}
 
-	requestInfo := &RequestInfo{
+	requestInfo := &requestInfo{
 		"DELETE",
 		p.url + "/things/" + t.Token,
 		authorization,
@@ -208,11 +209,11 @@ func (p proxy) Remove(authorization, ID string) error {
 	return p.mapErrorFromStatusCode(resp.StatusCode)
 }
 
-func (p proxy) getRemoteThingRepr(id, name string, configList []entities.Config) ThingProxyRepr {
-	return ThingProxyRepr{
+func (p thingProxy) getThingSchema(id, name string, configList []entities.Config) thingSchema {
+	return thingSchema{
 		Name: name,
-		Metadata: objMetadata{
-			Knot: objKnot{
+		Metadata: thingMetadata{
+			Thing: knotThing{
 				ID:     id,
 				Config: configList,
 			},
@@ -220,70 +221,31 @@ func (p proxy) getRemoteThingRepr(id, name string, configList []entities.Config)
 	}
 }
 
-func (p proxy) sendRequest(info *RequestInfo) (*http.Response, error) {
-	values, err := query.Values(info.options)
-	if err != nil {
-		return nil, err
-	}
-	queryString := "?" + values.Encode()
-
-	/**
-	 * Add Timeout in http.Client to avoid blocking the request.
-	 */
-	client := &http.Client{Timeout: 10 * time.Second}
-	req, err := http.NewRequest(info.method, info.url+queryString, bytes.NewBuffer(info.data))
-	if err != nil {
-		p.logger.Error(err)
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", info.authorization)
-	req.Header.Set("Content-Type", info.contentType)
-
-	return client.Do(req)
-}
-
-func (p proxy) mapErrorFromStatusCode(code int) error {
-	var err error
-
-	if code != http.StatusCreated {
-		switch code {
-		case http.StatusConflict:
-			err = errorConflict{}
-		case http.StatusUnauthorized:
-			err = entities.ErrThingUnauthorized
-		}
-	}
-	return err
-}
-
-func (p proxy) getPaginatedThings(authorization string) ([]*ThingProxyRepr, error) {
-	requestInfo := &RequestInfo{
+func (p thingProxy) getPaginatedThings(authorization string) ([]*thingSchema, error) {
+	requestInfo := &requestInfo{
 		"GET",
 		p.url + "/things",
 		authorization,
 		"application/json",
 		nil,
-		&RequestOptions{Limit: 100, Offset: 0}, // 100 is the max number of things that can be returned
+		&requestOptions{Limit: 100, Offset: 0}, // 100 is the max number of things that can be returned
 	}
 
-	var things []*ThingProxyRepr
+	var things []*thingSchema
 	keepGoing := true
 	for keepGoing {
 		resp, err := p.sendRequest(requestInfo)
 		if err != nil {
-			p.logger.Error(err)
 			return nil, err
 		}
 		defer resp.Body.Close()
 
 		err = p.mapErrorFromStatusCode(resp.StatusCode)
 		if err != nil {
-			p.logger.Error(err)
 			return nil, err
 		}
 
-		page := &pageFetchInput{}
+		page := &pageFetchSchema{}
 		err = json.NewDecoder(resp.Body).Decode(&page)
 		if err != nil {
 			return nil, err
@@ -298,4 +260,40 @@ func (p proxy) getPaginatedThings(authorization string) ([]*ThingProxyRepr, erro
 	}
 
 	return things, nil
+}
+
+func (p thingProxy) sendRequest(info *requestInfo) (*http.Response, error) {
+	values, err := query.Values(info.options)
+	if err != nil {
+		return nil, err
+	}
+	queryString := "?" + values.Encode()
+
+	/**
+	 * Add Timeout in http.Client to avoid blocking the request.
+	 */
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest(info.method, info.url+queryString, bytes.NewBuffer(info.data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", info.authorization)
+	req.Header.Set("Content-Type", info.contentType)
+
+	return client.Do(req)
+}
+
+func (p thingProxy) mapErrorFromStatusCode(code int) error {
+	var err error
+
+	if code != http.StatusCreated {
+		switch code {
+		case http.StatusConflict:
+			err = errorConflict{}
+		case http.StatusUnauthorized:
+			err = entities.ErrThingUnauthorized
+		}
+	}
+	return err
 }
