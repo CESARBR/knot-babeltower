33fdba3a37cbf51988cbcf181d45a6f8b9bd0467
Matheus <mtr@cesar.org.br>
2022-06-03 16:50:02 -0300

    Add timestamp inteface inside data struct

--- pkg/thing/entities/data.go
+++ pkg/thing/entities/data.go
@@ -2,6 +2,7 @@ package entities
 
 // Data represents the thing's data
 type Data struct {
-	SensorID int         `json:"sensorId"`
-	Value    interface{} `json:"value"`
+	SensorID  int         `json:"sensorId"`
+	Value     interface{} `json:"value"`
+	Timestamp interface{} `json:"timestamp"`
 }
